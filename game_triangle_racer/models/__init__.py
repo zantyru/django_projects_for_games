e75692ffc12a9d5e617d306dd8fbf4c0e42d586e
Evgeny Zaonegin <zaonegin_s@mail.ru>
2026-04-02 23:01:47 +0300

    Сортирует импорт модулей в алфавитном порядке

--- game_triangle_racer/models/__init__.py
+++ game_triangle_racer/models/__init__.py
@@ -1,30 +1,30 @@
 from game_triangle_racer.models.Config import Config
-from game_triangle_racer.models.ConfigOfInitialPlayerResource import ConfigOfInitialPlayerResource
 from game_triangle_racer.models.ConfigOfInitialPlayerCostume import ConfigOfInitialPlayerCostume
-from game_triangle_racer.models.Player import Player
-from game_triangle_racer.models.Resource import Resource
+from game_triangle_racer.models.ConfigOfInitialPlayerResource import ConfigOfInitialPlayerResource
 from game_triangle_racer.models.Costume import Costume
-from game_triangle_racer.models.Timer import Timer
-from game_triangle_racer.models.PlayerResource import PlayerResource
+from game_triangle_racer.models.Player import Player
 from game_triangle_racer.models.PlayerCostume import PlayerCostume
+from game_triangle_racer.models.PlayerResource import PlayerResource
 from game_triangle_racer.models.PlayerTimer import PlayerTimer
+from game_triangle_racer.models.Resource import Resource
 from game_triangle_racer.models.ShopPriceComponent import ShopPriceComponent
 from game_triangle_racer.models.ShopSet import ShopSet
 from game_triangle_racer.models.ShopSetComponent import ShopSetComponent
+from game_triangle_racer.models.Timer import Timer
 
 
 __all__ = [
     "Config",
-    "ConfigOfInitialPlayerResource",
     "ConfigOfInitialPlayerCostume",
+    "ConfigOfInitialPlayerResource",
     "Player",
+    "PlayerCostume",
+    "PlayerResource",
+    "PlayerTimer",
     "Resource",
     "Costume",
-    "Timer",
-    "PlayerResource",
-    "PlayerCostume",
-    "PlayerTimer",
     "ShopPriceComponent",
     "ShopSet",
     "ShopSetComponent",
+    "Timer",
 ]
